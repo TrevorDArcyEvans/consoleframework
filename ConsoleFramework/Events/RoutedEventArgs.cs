a7e66f122bf2c1c19a1258c9cb2b67655f643b16
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-30 17:21:24 +0100

    - code cleanup

--- ConsoleFramework/Events/RoutedEventArgs.cs
+++ ConsoleFramework/Events/RoutedEventArgs.cs
@@ -1,38 +1,37 @@
 using System;
 
-namespace ConsoleFramework.Events {
+namespace ConsoleFramework.Events
+{
+  public delegate void RoutedEventHandler(object sender, RoutedEventArgs e);
 
-    public delegate void RoutedEventHandler(object sender, RoutedEventArgs e);
+  public class RoutedEventArgs : EventArgs
+  {
+    private bool _handled;
 
-    public class RoutedEventArgs : EventArgs {
-        private bool handled;
-        private readonly object source;
-        private readonly RoutedEvent routedEvent;
-
-        public bool Handled {
-            get {
-                return handled;
-            }
-            set {
-                handled = value;
-            }
-        }
-
-        public object Source {
-            get {
-                return source;
-            }
-        }
-
-        public RoutedEvent RoutedEvent {
-            get {
-                return routedEvent;
-            }
-        }
-
-        public RoutedEventArgs (object source, RoutedEvent routedEvent) {
-            this.source = source;
-            this.routedEvent = routedEvent;
-        }
+    public bool Handled
+    {
+      get { return _handled; }
+      set { _handled = value; }
     }
+
+    private readonly object _source;
+
+    public object Source
+    {
+      get { return _source; }
+    }
+
+    private readonly RoutedEvent _routedEvent;
+
+    public RoutedEvent RoutedEvent
+    {
+      get { return _routedEvent; }
+    }
+
+    public RoutedEventArgs(object source, RoutedEvent routedEvent)
+    {
+      this._source = source;
+      this._routedEvent = routedEvent;
+    }
+  }
 }
