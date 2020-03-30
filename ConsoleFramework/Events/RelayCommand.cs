7740defa5967ce121b07398460c13aad75d9ca57
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-30 17:18:06 +0100

    - code cleanup

--- ConsoleFramework/Events/RelayCommand.cs
+++ ConsoleFramework/Events/RelayCommand.cs
@@ -2,40 +2,53 @@
 
 namespace ConsoleFramework.Events
 {
-    /// <summary>
-    /// Command constructed from delegates.
-    /// CanExecute = True by default if no canExecute functor provided.
-    /// </summary>
-    public class RelayCommand : ICommand
+  /// <summary>
+  /// Command constructed from delegates.
+  /// CanExecute = True by default if no canExecute functor provided.
+  /// </summary>
+  public class RelayCommand : ICommand
+  {
+    private readonly Action<object> _action;
+    private readonly Func<object, bool> _canExecute;
+
+    public RelayCommand(Action<object> action)
     {
-        private readonly Action< object > action;
-        private readonly Func< object, bool > canExecute;
+      if (null == action)
+      {
+        throw new ArgumentNullException("action");
+      }
 
-        public RelayCommand( Action< object > action ) {
-            if (null == action) throw new ArgumentNullException("action");
-            this.action = action;
-        }
-
-        public RelayCommand( Action< object > action, Func< object, bool > canExecute )
-            : this(action) {
-            if ( null == canExecute ) throw new ArgumentNullException( "canExecute" );
-            this.canExecute = canExecute;
-        }
-
-        public event EventHandler CanExecuteChanged;
-
-        public bool CanExecute( object parameter ) {
-            return canExecute == null || canExecute.Invoke( parameter );
-        }
-
-        public void Execute( object parameter ) {
-            if (CanExecute( parameter ))
-                action.Invoke( parameter );
-        }
-
-        public void RaiseCanExecuteChanged( ) {
-            if (CanExecuteChanged != null)
-                CanExecuteChanged.Invoke( this, EventArgs.Empty );
-        }
+      this._action = action;
     }
+
+    public RelayCommand(Action<object> action, Func<object, bool> canExecute)
+      : this(action)
+    {
+      if (null == canExecute)
+      {
+        throw new ArgumentNullException("canExecute");
+      }
+
+      this._canExecute = canExecute;
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+      return _canExecute == null || _canExecute.Invoke(parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+      if (CanExecute(parameter))
+        _action.Invoke(parameter);
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+      if (CanExecuteChanged != null)
+        CanExecuteChanged.Invoke(this, EventArgs.Empty);
+    }
+  }
 }
