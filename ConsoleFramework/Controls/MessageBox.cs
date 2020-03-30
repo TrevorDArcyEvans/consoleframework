a61a144bf200af2cce5bef5752131d37fad08fc1
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-30 15:17:47 +0100

    - code cleanup

--- ConsoleFramework/Controls/MessageBox.cs
+++ ConsoleFramework/Controls/MessageBox.cs
@@ -1,68 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using ConsoleFramework.Core;
 using ConsoleFramework.Events;
 
 namespace ConsoleFramework.Controls
 {
-    public delegate void MessageBoxClosedEventHandler(MessageBoxResult result);
+  public delegate void MessageBoxClosedEventHandler(MessageBoxResult result);
 
-    public class MessageBox : Window
+  public class MessageBox : Window
+  {
+    private readonly TextBlock _textBlock;
+
+    public MessageBox()
     {
-        private readonly TextBlock textBlock;
+      Panel panel = new Panel();
+      _textBlock = new TextBlock();
+      _textBlock.HorizontalAlignment = HorizontalAlignment.Center;
+      _textBlock.VerticalAlignment = VerticalAlignment.Center;
+      _textBlock.Margin = new Thickness(1);
+      Button button = new Button();
+      button.Margin = new Thickness(4, 0, 4, 0);
+      button.HorizontalAlignment = HorizontalAlignment.Center;
+      button.Caption = "OK";
+      button.OnClick += CloseButtonOnClicked;
+      panel.Children.Add(_textBlock);
+      panel.Children.Add(button);
+      panel.HorizontalAlignment = HorizontalAlignment.Center;
+      panel.VerticalAlignment = VerticalAlignment.Bottom;
+      this.Content = panel;
+    }
 
-        public MessageBox( ) {
-            Panel panel = new Panel();
-            textBlock = new TextBlock();
-            textBlock.HorizontalAlignment = HorizontalAlignment.Center;
-            textBlock.VerticalAlignment = VerticalAlignment.Center;
-            textBlock.Margin = new Thickness(1);
-            Button button = new Button(  );
-            button.Margin = new Thickness(4, 0, 4, 0);
-            button.HorizontalAlignment = HorizontalAlignment.Center;
-            button.Caption = "OK";
-            button.OnClick+=CloseButtonOnClicked;
-            panel.Children.Add( textBlock );
-            panel.Children.Add( button );
-            panel.HorizontalAlignment = HorizontalAlignment.Center;
-            panel.VerticalAlignment = VerticalAlignment.Bottom;
-            this.Content = panel;
-        }
+    protected virtual void CloseButtonOnClicked(object sender, RoutedEventArgs e)
+    {
+      Close();
+    }
 
-        protected virtual void CloseButtonOnClicked(object sender, RoutedEventArgs e)
+    public string Text
+    {
+      get { return _textBlock.Text; }
+      set { _textBlock.Text = value; }
+    }
+
+    public static void Show(string title, string text, MessageBoxClosedEventHandler onClosed)
+    {
+      var rootControl = ConsoleApplication.Instance.RootControl;
+      if (!(rootControl is WindowsHost))
+      {
+        throw new InvalidOperationException("Default windows host not found, create MessageBox manually");
+      }
+      var windowsHost = (WindowsHost) rootControl;
+      var messageBox = new MessageBox();
+      messageBox.Title = title;
+      messageBox.Text = text;
+      messageBox.AddHandler(ClosedEvent, new EventHandler((sender, args) =>
+      {
+        if (null != onClosed)
         {
-            Close();
+          onClosed(MessageBoxResult.Button1);
         }
-
-        public string Text {
-            get { return textBlock.Text; }
-            set { textBlock.Text = value; }
-        }
-
-        public static void Show( string title, string text, MessageBoxClosedEventHandler onClosed) {
-            Control rootControl = ConsoleApplication.Instance.RootControl;
-            if (!(rootControl is WindowsHost)) 
-                throw new InvalidOperationException("Default windows host not found, create MessageBox manually");
-            WindowsHost windowsHost = ( WindowsHost ) rootControl;
-            MessageBox messageBox = new MessageBox(  );
-            messageBox.Title = title;
-            messageBox.Text = text;
-            messageBox.AddHandler( ClosedEvent, new EventHandler(( sender, args ) => {
-                if ( null != onClosed ) {
-                    onClosed( MessageBoxResult.Button1 );
-                }
-            }) );
-            //messageBox.X =
-            windowsHost.ShowModal( messageBox );
-        }
+      }));
+      windowsHost.ShowModal(messageBox);
     }
-
-    public enum MessageBoxResult
-    {
-        Button1,
-        Button2,
-        Button3
-    }
+  }
 }
---
+++ ConsoleFramework/Controls/MessageBoxResult.cs
@@ -0,0 +1,9 @@
+namespace ConsoleFramework.Controls
+{
+  public enum MessageBoxResult
+  {
+    Button1,
+    Button2,
+    Button3
+  }
+}
