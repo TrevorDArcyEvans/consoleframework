e9f70ecb19212c96c28a40f8e09ec068e454aeb7
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-30 18:00:26 +0100

    - code cleanup

--- Xaml/RefMarkupExtension.cs
+++ Xaml/RefMarkupExtension.cs
@@ -2,40 +2,47 @@
 
 namespace Xaml
 {
-    /// <summary>
-    /// Returns an object that is referenced in expression. Example:
-    /// {Ref myObject} will return object with x:Id="myObject".
-    /// Forward-references are supported too.
-    /// </summary>
-    [MarkupExtension("Ref")]
-    class RefMarkupExtension : IMarkupExtension
+  /// <summary>
+  /// Returns an object that is referenced in expression. Example:
+  /// {Ref myObject} will return object with x:Id="myObject".
+  /// Forward-references are supported too.
+  /// </summary>
+  [MarkupExtension("Ref")]
+  internal class RefMarkupExtension : IMarkupExtension
+  {
+    public RefMarkupExtension()
     {
-        public RefMarkupExtension( ) {
-        }
-
-        public RefMarkupExtension( string @ref ) {
-            Ref = @ref;
-        }
-
-        /// <summary>
-        /// String reference to ID of object to be used.
-        /// </summary>
-        public String Ref { get; set; }
-
-        public object ProvideValue( IMarkupExtensionContext context ) {
-            if (string.IsNullOrEmpty( Ref ))
-                throw new InvalidOperationException("Ref is null or empty string.");
-            
-            object obj = context.GetObjectById( Ref );
-            if ( null == obj ) {
-                if ( context.IsFixupTokenAvailable ) {
-                    return context.GetFixupToken( new string[ ] { Ref } );
-                } else {
-                    throw new InvalidOperationException(string.Format("Object with Id={0} not found.", Ref));
-                }
-            }
-
-            return obj;
-        }
     }
+
+    public RefMarkupExtension(string @ref)
+    {
+      Ref = @ref;
+    }
+
+    /// <summary>
+    /// String reference to ID of object to be used.
+    /// </summary>
+    public String Ref { get; set; }
+
+    public object ProvideValue(IMarkupExtensionContext context)
+    {
+      if (string.IsNullOrEmpty(Ref))
+      {throw new InvalidOperationException("Ref is null or empty string.");}
+
+      var obj = context.GetObjectById(Ref);
+      if (null == obj)
+      {
+        if (context.IsFixupTokenAvailable)
+        {
+          return context.GetFixupToken(new string[] { Ref });
+        }
+        else
+        {
+          throw new InvalidOperationException(string.Format("Object with Id={0} not found.", Ref));
+        }
+      }
+
+      return obj;
+    }
+  }
 }
