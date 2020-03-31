d620e4cfd4d683098578ba1cb392bde8eae73ac3
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-31 14:03:16 +0100

    - code cleanup

--- Binding/Validators/RequiredValidator.cs
+++ Binding/Validators/RequiredValidator.cs
@@ -2,14 +2,19 @@
 
 namespace Binding.Validators
 {
-    /// <summary>
-    /// Validator checks the value is not null or empty (if value string).
-    /// </summary>
-    public class RequiredValidator : IBindingValidator {
-        public ValidationResult Validate(Object value) {
-            if (value == null || value is String && ((String) value).Length == 0)
-                return new ValidationResult(false, "Value is required");
-            return new ValidationResult(true);
-        }
+  /// <summary>
+  /// Validator checks the value is not null or empty (if value string).
+  /// </summary>
+  public class RequiredValidator : IBindingValidator
+  {
+    public ValidationResult Validate(Object value)
+    {
+      if (value == null || value is string && ((string) value).Length == 0)
+      {
+        return new ValidationResult(false, "Value is required");
+      }
+
+      return new ValidationResult(true);
     }
+  }
 }
