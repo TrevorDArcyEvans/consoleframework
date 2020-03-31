b4b0b680dc332d797cb97745fdabb43d9b4ab708
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-31 13:08:04 +0100

    - code cleanup

--- Xaml/MarkupExtensionAttribute.cs
+++ Xaml/MarkupExtensionAttribute.cs
@@ -2,20 +2,22 @@
 
 namespace Xaml
 {
-    /// <summary>
-    /// Attribute marks the markup extensions and specifies the names
-    /// by which they will be available in XAML.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
-    public class MarkupExtensionAttribute : Attribute
+  /// <summary>
+  /// Attribute marks the markup extensions and specifies the names
+  /// by which they will be available in XAML.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+  public class MarkupExtensionAttribute : Attribute
+  {
+    public MarkupExtensionAttribute()
     {
-        public MarkupExtensionAttribute( ) {
-        }
-
-        public MarkupExtensionAttribute( string name ) {
-            Name = name;
-        }
-
-        public String Name { get; set; }
     }
+
+    public MarkupExtensionAttribute(string name)
+    {
+      Name = name;
+    }
+
+    public string Name { get; set; }
+  }
 }
