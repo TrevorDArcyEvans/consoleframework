fe49a32c710ca824baf67f03be6b8ac6d3d7f82a
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-31 12:14:29 +0100

    - code cleanup

--- ConsoleFramework/Xaml/BindingMarkupExtension.cs
+++ ConsoleFramework/Xaml/BindingMarkupExtension.cs
@@ -7,53 +7,66 @@ using Xaml;
 
 namespace ConsoleFramework.Xaml
 {
-    [MarkupExtension("Binding")]
-    class BindingMarkupExtension : IMarkupExtension
+  [MarkupExtension("Binding")]
+  internal class BindingMarkupExtension : IMarkupExtension
+  {
+    public BindingMarkupExtension()
     {
-        public BindingMarkupExtension() {
-        }
-
-        public BindingMarkupExtension(string path) {
-            Path = path;
-        }
-
-        public String Path { get; set; }
-
-        public String Mode { get; set; }
-
-        public Object Source { get; set; }
-
-        /// <summary>
-        /// Converter to be used.
-        /// </summary>
-        public IBindingConverter Converter { get; set; }
-
-        public object ProvideValue(IMarkupExtensionContext context) {
-            Object realSource = Source ?? context.DataContext;
-            if ( null != realSource && !( realSource is INotifyPropertyChanged ) ) {
-                throw new ArgumentException("Source must be INotifyPropertyChanged to use bindings");
-            }
-            if (null != realSource) {
-                BindingMode mode = BindingMode.Default;
-                if ( Path != null ) {
-                    Type enumType = typeof ( BindingMode );
-                    string[ ] enumNames = enumType.GetTypeInfo().GetEnumNames( );
-                    for ( int i = 0, len = enumNames.Length; i < len; i++ ) {
-                        if ( enumNames[ i ] == Mode ) {
-                            mode = ( BindingMode ) Enum.ToObject( enumType, enumType.GetTypeInfo().GetEnumValues( ).GetValue( i ) );
-                            break;
-                        }
-                    }
-                }
-                BindingBase binding = new BindingBase( context.Object, context.PropertyName,
-                    (INotifyPropertyChanged) realSource, Path, mode);
-                if ( Converter != null )
-                    binding.Converter = Converter;
-                binding.Bind(  );
-                // mb return actual property value ?
-                return null;
-            }
-            return null;
-        }
     }
+
+    public BindingMarkupExtension(string path)
+    {
+      Path = path;
+    }
+
+    public string Path { get; set; }
+
+    public string Mode { get; set; }
+
+    public object Source { get; set; }
+
+    /// <summary>
+    /// Converter to be used.
+    /// </summary>
+    public IBindingConverter Converter { get; set; }
+
+    public object ProvideValue(IMarkupExtensionContext context)
+    {
+      var realSource = Source ?? context.DataContext;
+      if (null != realSource && !(realSource is INotifyPropertyChanged))
+      {
+        throw new ArgumentException("Source must be INotifyPropertyChanged to use bindings");
+      }
+
+      if (null != realSource)
+      {
+        var mode = BindingMode.Default;
+        if (Path != null)
+        {
+          var enumType = typeof(BindingMode);
+          var enumNames = enumType.GetTypeInfo().GetEnumNames();
+          for (int i = 0, len = enumNames.Length; i < len; i++)
+          {
+            if (enumNames[i] == Mode)
+            {
+              mode = (BindingMode) Enum.ToObject(enumType, enumType.GetTypeInfo().GetEnumValues().GetValue(i));
+              break;
+            }
+          }
+        }
+
+        var binding = new BindingBase(context.Object, context.PropertyName, (INotifyPropertyChanged) realSource, Path, mode);
+        if (Converter != null)
+        {
+          binding.Converter = Converter;
+        }
+
+        binding.Bind();
+        // mb return actual property value ?
+        return null;
+      }
+
+      return null;
+    }
+  }
 }
