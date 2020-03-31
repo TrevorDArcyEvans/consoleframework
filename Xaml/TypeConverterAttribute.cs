43ed63471dd3444d6c08625acd315f6799d74b7d
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-31 13:16:03 +0100

    - code cleanup

--- Xaml/TypeConverterAttribute.cs
+++ Xaml/TypeConverterAttribute.cs
@@ -5,8 +5,7 @@ namespace Xaml
   /// <summary>
   /// Specifies type of converter to be used to convert from or to current type.
   /// </summary>
-  [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct
-                                         | AttributeTargets.Enum, Inherited = true)]
+  [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum, Inherited = true)]
   public class TypeConverterAttribute : Attribute
   {
     public TypeConverterAttribute()
