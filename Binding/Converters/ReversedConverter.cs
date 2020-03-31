9fca642d5c428e25431ce8f427c3d6fc37f35b10
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-31 13:53:57 +0100

    - code cleanup

--- Binding/Converters/ReversedConverter.cs
+++ Binding/Converters/ReversedConverter.cs
@@ -2,27 +2,33 @@
 
 namespace Binding.Converters
 {
-    public class ReversedConverter : IBindingConverter {
-        readonly IBindingConverter converter;
+  public class ReversedConverter : IBindingConverter
+  {
+    private readonly IBindingConverter _converter;
 
-        public ReversedConverter(IBindingConverter converter) {
-            this.converter = converter;
-        }
-
-        public Type FirstType {
-            get { return converter.SecondType; }
-        }
-
-        public Type SecondType {
-            get { return converter.FirstType; }
-        }
-
-        public ConversionResult Convert(object tFirst) {
-            return converter.ConvertBack(tFirst);
-        }
-
-        public ConversionResult ConvertBack(object tSecond) {
-            return converter.Convert(tSecond);
-        }
+    public ReversedConverter(IBindingConverter converter)
+    {
+      this._converter = converter;
     }
+
+    public Type FirstType
+    {
+      get { return _converter.SecondType; }
+    }
+
+    public Type SecondType
+    {
+      get { return _converter.FirstType; }
+    }
+
+    public ConversionResult Convert(object tFirst)
+    {
+      return _converter.ConvertBack(tFirst);
+    }
+
+    public ConversionResult ConvertBack(object tSecond)
+    {
+      return _converter.Convert(tSecond);
+    }
+  }
 }
