addfafbfce39be46cf39acd3a94d2cd1ecb1c5e9
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-31 13:53:14 +0100

    - code cleanup

--- Binding/Converters/ConversionResult.cs
+++ Binding/Converters/ConversionResult.cs
@@ -2,38 +2,42 @@
 
 namespace Binding.Converters
 {
-    /// <summary>
-    /// Represents value conversion result.
-    /// </summary>
-    public class ConversionResult
+  /// <summary>
+  /// Represents value conversion result.
+  /// </summary>
+  public class ConversionResult
+  {
+    private readonly object _value;
+
+    public object Value
     {
-        private readonly Object value;
-        private readonly bool success;
-        private readonly String failReason;
-
-        public object Value {
-            get { return value; }
-        }
-
-        public bool Success {
-            get { return success; }
-        }
-
-        public string FailReason {
-            get { return failReason; }
-        }
-
-        public ConversionResult(Object value)
-        {
-            this.value = value;
-            this.success = true;
-        }
-
-        public ConversionResult(bool success, String failReason)
-        {
-            this.success = success;
-            this.failReason = failReason;
-        }
+      get { return _value; }
     }
 
+    private readonly bool _success;
+
+    public bool Success
+    {
+      get { return _success; }
+    }
+
+    private readonly string _failReason;
+
+    public string FailReason
+    {
+      get { return _failReason; }
+    }
+
+    public ConversionResult(object value)
+    {
+      this._value = value;
+      this._success = true;
+    }
+
+    public ConversionResult(bool success, String failReason)
+    {
+      this._success = success;
+      this._failReason = failReason;
+    }
+  }
 }
