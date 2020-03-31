6fcc90da801e24ee4586e12b7cd8086e55e4e61c
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-03-31 14:03:56 +0100

    - code cleanup

--- Binding/Validators/ValidationResult.cs
+++ Binding/Validators/ValidationResult.cs
@@ -2,32 +2,34 @@
 
 namespace Binding.Validators
 {
-    /// <summary>
-    /// Represents the result of data binding validation.
-    /// </summary>
-    public class ValidationResult
+  /// <summary>
+  /// Represents the result of data binding validation.
+  /// </summary>
+  public class ValidationResult
+  {
+    private readonly bool _valid;
+
+    public bool Valid
     {
-        private readonly bool valid;
-        private readonly String message;
-
-        public bool Valid {
-            get { return valid; }
-        }
-
-        public string Message {
-            get { return message; }
-        }
-
-        public ValidationResult(bool valid)
-        {
-            this.valid = valid;
-        }
-
-        public ValidationResult(bool valid, String message)
-        {
-            this.valid = valid;
-            this.message = message;
-        }
+      get { return _valid; }
     }
 
+    private readonly string _message;
+
+    public string Message
+    {
+      get { return _message; }
+    }
+
+    public ValidationResult(bool valid)
+    {
+      this._valid = valid;
+    }
+
+    public ValidationResult(bool valid, String message)
+    {
+      this._valid = valid;
+      this._message = message;
+    }
+  }
 }
